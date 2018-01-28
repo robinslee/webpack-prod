0491400d4df595eecf7960f2d819d2ea0ede478f
robinslee <robinslee@126.com>
2018-01-28 20:45:33 +0800

    add comments

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,7 @@
 const merge = require("webpack-merge");
 const common = require("./webpack.common.js");
 
+// inline-* or evel-* can increase bundle size and reduce performance
 module.exports = merge(common, {
     devtool: "inline-source-map",
     devServer: {
